96097af2fa8b1fc8536ca9400bc7ba170037f53b
[Lewis Burgess] <[lewis.burgess19@yahoo.com]>
2024-08-14 17:15:37 +0100

    still trying to filter search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import SearchResults from "../SearchResults/searchresults";
 import Playlist from "../Playlist/playlist";
 import SearchBar from "../SearchBar/searchbar";
-import {Spotify} from "../util/spotify";
+import { Spotify } from "../util/spotify";
 
-
-function App () {
-
+function App() {
+  // State for storing the results from a search query
   const [searchResults, setSearchResults] = useState([
     {
       name: "Example track name 1",
@@ -20,13 +19,16 @@ function App () {
       artist: "Example track artist 2",
       album: "Example track album 2",
       id: 2,
-    }]
-  );
+    },
+  ]);
 
+  // State for the name of the playlist being created or modified
   const [playlistName, setPlaylistName] = useState("Example playlist Name");
 
+  // State for storing the filtered search results
   const [userSearchResults, setUserSearchResults] = useState([]);
 
+  // State for storing the tracks currently in the playlist
   const [playlistTracks, setPlaylistTracks] = useState([
     {
       name: "Example playlist Name 1",
@@ -39,77 +41,89 @@ function App () {
       artist: "Example Playlist Artist 2",
       album: "Example Playlist Album 2",
       id: 2,
-    }
+    },
   ]);
 
+  // Function to add a track to the playlist
   function addTrack(track) {
     // Check if the track already exists in the playlist
     const existingTrack = playlistTracks.find((t) => t.id === track.id);
-  
+
     if (existingTrack) {
       console.log("Track already exists");
     } else {
-      // Add the track to the playlist
-      const newTrackList = playlistTracks.concat(track);
+      // Add the track to the playlist using the spread operator
+      const newTrackList = [...playlistTracks, track];
       setPlaylistTracks(newTrackList);
-  
+
       // Filter out the added track from the search results
       const updatedSearchResults = userSearchResults.filter(
         (searchTrack) => searchTrack.id !== track.id
       );
-      setUserSearchResults(updatedSearchResults); // assuming there's a setter for user search results
+
+      // Update the search results state
+      setUserSearchResults(updatedSearchResults);
     }
   }
-  
 
+  // Function to remove a track from the playlist
   function removeTrack(track) {
-    const existingTrack = playlistTracks.filter((t) => t.id !== track.id);
-    setPlaylistTracks(existingTrack);
-  };
+    // Filter the playlist to exclude the track to be removed
+    const updatedTrackList = playlistTracks.filter((t) => t.id !== track.id);
+    setPlaylistTracks(updatedTrackList);
+  }
 
+  // Function to update the playlist name
   function updatePlaylistName(name) {
     setPlaylistName(name);
   }
 
+  // Function to save the playlist to Spotify
   function savePlaylist() {
-    const trackURIs = playlistTracks.map((t) => t.uri);
-      alert(`${playlistName} was added to your spotify account.`)
-      Spotify.savePlaylist(playlistName, trackURIs).then(() => {
-        updatePlaylistName("New Playlist");
-        setPlaylistTracks([]);
-      });
-    
-     
+    const trackURIs = playlistTracks.map((t) => t.uri); // Map tracks to their URIs
+
+    // Notify the user that the playlist has been saved (this line is for demonstration)
+    alert(`${playlistName} was added to your Spotify account.`);
+
+    // Call the Spotify API to save the playlist
+    Spotify.savePlaylist(playlistName, trackURIs).then(() => {
+      // Reset playlist name and tracks after saving
+      updatePlaylistName("New Playlist");
+      setPlaylistTracks([]);
+    });
   }
 
+  // Function to search for tracks using the Spotify API
   function search(term) {
     Spotify.search(term).then((result) => setSearchResults(result));
-    console.log(term);
+    console.log(term); // Log the search term (for debugging)
   }
 
-    return (
-        <div>
-        <h1>
-          Riddi<span className={styles.highlight}>mmm</span>Selecta
-        </h1>
-        <div className={styles.App}>
-          {/* <!-- Add a SearchBar component --> */}
-          <SearchBar onSearch={search}/>
-          
-          <div className={styles['App-playlist']}>
-            {/* <!-- Add a SearchResults component --> */}
-            <SearchResults userSearchResults={searchResults} onAdd={addTrack}/>
-            {/* <!-- Add a Playlist component --> */}
-            <Playlist 
-              playlistName={playlistName} 
-              playlistTracks={playlistTracks}
-              onRemove={removeTrack}
-              onNameChange={updatePlaylistName}
-              onSave={savePlaylist}/>
-          </div>
+  return (
+    <div>
+      <h1>
+        Riddi<span className={styles.highlight}>mmm</span>Selecta
+      </h1>
+      <div className={styles.App}>
+        {/* Add a SearchBar component to allow the user to search for tracks */}
+        <SearchBar onSearch={search} />
+
+        <div className={styles['App-playlist']}>
+          {/* Add a SearchResults component to display the search results */}
+          <SearchResults userSearchResults={searchResults} onAdd={addTrack} />
+
+          {/* Add a Playlist component to display the current playlist */}
+          <Playlist
+            playlistName={playlistName}
+            playlistTracks={playlistTracks}
+            onRemove={removeTrack}
+            onNameChange={updatePlaylistName}
+            onSave={savePlaylist}
+          />
         </div>
       </div>
-        );
+    </div>
+  );
 }
 
 export default App;
